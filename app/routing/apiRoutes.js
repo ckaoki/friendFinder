3030c545532a1a9e2994f093bd33e37ba43f97f9
ckaoki <cullan_aoki@msn.com>
2019-07-09 17:32:33 -0700

    app is fully functional. completed modal window.  Also created a survey.js to hold survey.html script.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,9 +9,26 @@ router.get("/friends", function(req, res) {
 });
   
 
-router.post("/friends", function(req, res) {
+router.post("/friends", function(req, res) {  
+    // change scores from strings to numbers  
+    req.body.scores = req.body.scores.map(Number);
+    
+    // find closest matching friend
+    var differences = []
+    friends.forEach(friend => {
+        var totalDifference = 0;
+        console.log(friend);
+        for(var i=0; i<req.body.scores.length; i++ ){
+            totalDifference += Math.abs(req.body.scores[i]-friend.scores[i]);
+        };
+        differences.push(totalDifference);
+    });
+    var closestFriend = friends[differences.indexOf(Math.min.apply(null, differences))];
+    console.log(closestFriend);
+
+    // push on to friends array and send
     friends.push(req.body);
-    res.json(true);
+    res.send(closestFriend);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var express = require('express');
 var path = require('path')
 var apiRoutes = require('./app/routing/apiRoutes');
 var htmlRoutes = require('./app/routing/htmlRoutes');
-
 var app = express();
-
 var PORT = process.env.PORT || 3000;
+
 app.use('/images', express.static(__dirname +'/app/public/images'));  
+app.use('/javascript', express.static(__dirname +'/app/javascript'));  
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
